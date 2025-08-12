c897603123fbc51b7031ccd1dfc8c2ce388a66dc
Niv Pagir <nivpgir@gmail.com>
2025-08-13 01:50:03 +0300

    httpimport works!
    There was a bug where `exec` wouldn't accept a `bytes` object, which
    was fixed by decoding the bytes to `str` in python, the `exec` works
    and `remote_repo` works!!!

--- python/main.py
+++ python/main.py
@@ -1,3 +1,10 @@
-import pyby_main
-print(pyby_main)
-print("LULA!")
+import httpimport
+
+url = "https://gist.githubusercontent.com/operatorequals/ee5049677e7bbc97af2941d1d3f04ace/raw/e55fa867d3fb350f70b2897bb415f410027dd7e4"
+
+with httpimport.remote_repo(url):
+  import hello
+hello.hello()
+
+mod = httpimport.load('hello', url=url)
+mod.hello()
